03770765d24ff7f377d9a1d3332111b3c1b2f889
ariarangajanan <arangajanan.nar@gmail.com>
2024-04-23 18:26:31 +0530

    project commit 4

--- TravelerHelperAdmin/src/main/java/com/MITProjectAdmin/AdminTravelerHelperApplication.java
+++ TravelerHelperAdmin/src/main/java/com/MITProjectAdmin/AdminTravelerHelperApplication.java
@@ -9,7 +9,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
-@ComponentScan(basePackages = {"com.MITProjectService"})
+@ComponentScan(basePackages = {"com.MITProjectAdmin","com.MITProjectService"})
 public class AdminTravelerHelperApplication {
 
 	public static void main(String[] args) {
