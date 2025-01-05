7d132c1fb97faaf2af790d2ddfb43f135cc3188d
ariarangajanan <arangajanan.nar@gmail.com>
2025-01-06 00:17:17 +0530

    Booking approval and reject Impls: need to null fixes

---
+++ TravelerHelperService/src/main/java/com/MITProjectService/bot/dao/jpaRepos/PaymentRepository.java
@@ -0,0 +1,1 @@
+package com.MITProjectService.bot.dao.jpaRepos;import com.MITProjectService.bot.domain.Payment;import org.springframework.data.jpa.repository.JpaRepository;import org.springframework.stereotype.Repository;@Repositorypublic interface PaymentRepository extends JpaRepository<Payment, Long> {}
