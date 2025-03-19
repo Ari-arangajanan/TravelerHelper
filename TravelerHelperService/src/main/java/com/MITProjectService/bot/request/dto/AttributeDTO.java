f60d0df7d2d21a74b0a44ad53e5cfb6bf8f12754
ariarangajanan <arangajanan.nar@gmail.com>
2025-03-20 02:01:40 +0530

    update Service impl

--- TravelerHelperService/src/main/java/com/MITProjectService/bot/request/dto/AttributeDTO.java
+++ TravelerHelperService/src/main/java/com/MITProjectService/bot/request/dto/AttributeDTO.java
@@ -1,1 +1,1 @@
-package com.MITProjectService.bot.request.dto;import lombok.Data;@Datapublic class AttributeDTO {    private String attributeName;    private String attributeValue;}
+package com.MITProjectService.bot.request.dto;import lombok.Data;@Datapublic class AttributeDTO {    private Long id;    private String attributeName;    private String attributeValue;}
